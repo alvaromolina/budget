93f5910acb27f8f4d6e8283ade710ff5cc417a0e
Alvaro Molina <alvaromolina@Alvaros-MacBook-Pro.local>
2023-11-29 11:35:24 -0400

    compile errores fix

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,7 +14,7 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-10 flex h-14  px-4">
-        <MainNav className="mx-6" />
+        <MainNav />
 
         <div className="ml-auto flex items-center space-x-4">
             <UserNav />
